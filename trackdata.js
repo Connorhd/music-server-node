a5af315b275967a1c4eca3cb48abfd0a4d28472f
Alex Wilson <mrwilson@sabriel.(none)>
2011-01-07 16:52:05 +0000

    Tweaked trackdata.js, contains full output for debugging.

--- trackdata.js
+++ trackdata.js
@@ -5,27 +5,37 @@ var exec = require('child_process').exec;
 
 exports.getInfo = function(path, callback){
 
-	exec("mplayer -frames 0 "+path, function (error, stdout, stderr) {		//Mplayer the file and stop it, collect data from stdout
-
-			if (error) {
-			sys.puts("Error: "+error);
-			}
+	exec("mplayer -frames 0 "+path, function (error, stdout, stderr) {		
+	//Mplayer the file and stop it, collect data from stdout
+		
+		if (error) {
+		sys.puts("Error: "+error);
+		}
+		
+		else {
+			//Regex the stdout to find track data
+			//The /i is necessary for case-insensitivity.
+			var get = {	"track": /(Title|Song|Name): (.*)/i,			
+					"artist": /(Singer|Band|Artist|Author): (.*)/i,
+					"album": /(Album): (.*)/i,
+					"year": /(Year|Release): (.*)/i
+			};
 			
-			else {
-				var get = {	"track": /(Title|Song|Name): (.*)/i,			//Regex the stdout to find track data
-						"artist": /(Singer|Band|Artist|Author): (.*)/i,		//The /i is necessary for case-insensitivity.
-						"album": /(Disc|Album|CD): (.*)/i,
-						"year": /(Year|Release): (.*)/i
-				};
-
-				var data = {};
-				for (var i in get) {
-					if (get[i].exec(stdout) !== null) {			//If data exists for the field
-						data[i] = get[i].exec(stdout)[2];			//Add it to the array
-					}
+			var data = {};
+			
+			for (var i in get) {
+		
+				//If data exists for the field, add it to the array.
+		
+				if (get[i].exec(stdout) !== null) {		
+					data[i] = get[i].exec(stdout)[2];	
 				}
-			
-				callback(data);							//Run the callback function on the array.
 			}
+
+			data["output"] = stdout;
+
+			//Run the callback function on the array.
+			callback(null, data);
+		}
 	});
 }
